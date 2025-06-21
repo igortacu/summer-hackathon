9a0d1aa85d9d048b06a3f360c0df853df79294fb
Ina Pancenco <inapancenco00@gmail.com>
2025-06-21 16:30:14 +0300

    added role management to database

--- Backend/database.py
+++ Backend/database.py
@@ -5,7 +5,8 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
-        "group" TEXT NOT NULL,
+        "Academic group" TEXT NOT NULL,
+        "PBL group" TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         role TEXT NOT NULL,
         password TEXT NOT NULL
@@ -15,15 +16,17 @@ conn.commit()
 def sign_in(name, group, email, role, password):
     cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
     user = cursor.fetchone()
-
     if user:
         print(f"User with email '{email}' already exists.")
     else:
-        cursor.execute('''
-            INSERT INTO users (name, "group", email, role, password)
-            VALUES (?, ?, ?, ?, ?)
-        ''', (name, group, email, role, password))
-        conn.commit()
-        print(f"User '{name}' signed in and added to database.")
-
-conn.close()
+        cursor.execute("SELECT * FROM users WHERE role = ?", (role,))
+        role_taken = cursor.fetchone()
+        if role_taken:
+            print(f"The role '{role}' is already taken. Please select another role.")
+        else:
+            cursor.execute('''
+                INSERT INTO users (name, "Academic group", "PBL group", email, role, password)
+                VALUES (?, ?, '', ?, ?, ?)
+            ''', (name, group, email, role, password))
+            conn.commit()
+            print(f"User '{name}' signed in and added to database.")
